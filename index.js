df82a00109500ea6ac588cf005a38998d24e30c7
Kenneth Falck <kennu@iki.fi>
2016-03-27 00:32:57 +0200

    Fix compatibility with Serverless 0.5.x and add function autoprune command

--- index.js
+++ index.js
@@ -5,27 +5,27 @@
  * Kenneth Falck <kennu@iki.fi> 2016
  */
 
-module.exports = function(ServerlessPlugin, serverlessPath) { // Always pass in the ServerlessPlugin Class
-
+module.exports = function(S) {
   const path    = require('path'),
       fs        = require('fs'),
-      SUtils    = require(path.join(serverlessPath, 'utils/index')),
-      SCli      = require(path.join(serverlessPath, 'utils/cli')),
+      SUtils    = require(S.getServerlessPath('utils')),
+      SCli      = require(S.getServerlessPath('utils/cli')),
+      SError    = require(S.getServerlessPath('Error')),
       BbPromise = require('bluebird'); // Serverless uses Bluebird Promises and we recommend you do to because they provide more than your average Promise :)
 
   /**
    * ServerlessPluginAutoprune
    */
 
-  class ServerlessPluginAutoprune extends ServerlessPlugin {
+  class ServerlessPluginAutoprune extends S.classes.Plugin {
 
     /**
      * Constructor
      * - Keep this and don't touch it unless you know what you're doing.
      */
 
-    constructor(S) {
-      super(S);
+    constructor() {
+      super();
     }
 
     /**
@@ -46,6 +46,34 @@ module.exports = function(ServerlessPlugin, serverlessPath) { // Always pass in 
      */
 
     registerActions() {
+      S.addAction(this.autoprune.bind(this), {
+        handler:        'autoprune',
+        description:    'Automatically deletes old Lambda function versions not attached to a stage',
+        context:        'function',
+        contextAction:  'autoprune',
+        options:        [
+          {
+            option:      'stage',
+            shortcut:    's',
+            description: 'Optional if only one stage is defined in project'
+          }, {
+            option:      'region',
+            shortcut:    'r',
+            description: 'Optional - Target one region to deploy to'
+          }, {
+            option:      'all',
+            shortcut:    'a',
+            description: 'Autoprune all functions'
+          }
+        ],
+        parameters:     [
+          {
+            parameter:   'function',
+            description: 'Function to autoprune (use -a for all)',
+            position:    '0->'
+          }
+        ]
+      });
       return BbPromise.resolve();
     }
 
@@ -56,7 +84,7 @@ module.exports = function(ServerlessPlugin, serverlessPath) { // Always pass in 
      */
 
     registerHooks() {
-      this.S.addHook(this._hookPostFunctionDeploy.bind(this), {
+      S.addHook(this._hookPostFunctionDeploy.bind(this), {
         action: 'functionDeploy',
         event:  'post'
       });
@@ -64,18 +92,81 @@ module.exports = function(ServerlessPlugin, serverlessPath) { // Always pass in 
       return BbPromise.resolve();
     }
 
+    autoprune(evt) {
+      var promise = BbPromise.resolve();
+      var project = S.getProject();
+      var functions = project.functions;
+      var found = 0;
+      var stage = evt.options.stage;
+      var region = evt.options.region;
+
+      if (!evt.options.all && !evt.options.function.length) {
+        return BbPromise.reject(new SError('Function name or -a required'));
+      }
+
+      if (stage && !project.validateStageExists(stage)) {
+        return BbPromise.reject(new SError(`Stage ${stage} doesnt exist in this project!`))
+      }
+
+      return BbPromise.resolve()
+      .then(() => {
+        if (!stage) {
+          return this.cliPromptSelectStage('Function Autoprune - Choose a stage: ', stage, false)
+          .then(aStage => {
+            stage = aStage;
+          });
+        }
+      })
+      .then(() => {
+
+        if (stage && region && !proj.validateRegionExists(stage, region)) {
+          return BbPromise.reject(new SError(`Region ${region} doesnt exist in stage ${stage}!`))
+        }
+
+        // Select default region in stage if not specified
+        if (stage && !region) {
+          var stageObj = project.getStage(stage);
+          var regions = stageObj.getAllRegions();
+          if (regions.length == 1) {
+            region = regions[0].getName();
+          } else {
+            return BbPromise.reject(new SError('Region has multiple stages, specify one with -r'));
+          }
+        }
+
+        Object.keys(functions).map((functionName) => {
+          if (evt.options.all || evt.options.function.indexOf(functionName) >= 0) {
+            found += 1;
+            promise = promise.then(() => {
+              return this.autopruneFunction({stage:stage, region:region}, functions[functionName]);
+            });
+          }
+        });
+        if (evt.options.function.length && found < evt.options.function.length) {
+          return BbPromise.reject(new SError('Function not found'));
+        }
+
+        // return this.autopruneFunction({stage:evt.options.stage, region:evt.options.region}, func)
+        return promise;
+      })
+      .then(() => {
+        return evt;
+      });
+    }
+
     /**
      * Post deploy prune hook
      * - Be sure to ALWAYS accept and return the "evt" object, or you will break the entire flow.
      * - The "evt" object contains Action-specific data.  You can add custom data to it, but if you change any data it will affect subsequent Actions and Hooks.
-     * - You can also access other Project-specific data @ this.S Again, if you mess with data on this object, it could break everything, so make sure you know what you're doing ;)
+     * - You can also access other Project-specific data @ S Again, if you mess with data on this object, it could break everything, so make sure you know what you're doing ;)
      */
 
     _hookPostFunctionDeploy(evt) {
       var self = this;
       var promise = BbPromise.resolve();
       Object.keys(evt.data.deployed || []).map(function (region) {
-        evt.data.deployed[region].map(function (func) {
+        evt.data.deployed[region].map(function (deployed) {
+          var func = S.getProject().getFunction(deployed.functionName);
           promise = promise.then(function () {
             return self.autopruneFunction({stage:evt.options.stage, region:region}, func)
           });
@@ -131,21 +222,22 @@ module.exports = function(ServerlessPlugin, serverlessPath) { // Always pass in 
     autopruneFunction(options, func) {
       var self = this;
       var AWS = require('aws-sdk');
-      var lambda = new AWS.Lambda({region:options.region, accessKeyId:this.S.config.awsAdminKeyId, secretAccessKey:this.S.config.awsAdminSecretKey});
+      var functionName = func.getDeployedName(options);
+      var lambda = new AWS.Lambda({region:options.region, accessKeyId:S.config.awsAdminKeyId, secretAccessKey:S.config.awsAdminSecretKey});
       var versions;
       var aliases;
       var aliasMap = {};
       BbPromise.promisifyAll(lambda, {suffix:'Promise'});
-      return self.listAllVersions(lambda, func.functionName)
+      return self.listAllVersions(lambda, functionName)
       .then(function (aVersions) {
         versions = aVersions;
-        return self.listAllAliases(lambda, func.functionName);
+        return self.listAllAliases(lambda, functionName);
       })
       .then(function (aAliases) {
         aliases = aAliases;
         if (!aliases.length) {
           // No aliases, don't autoprune everything
-          SCli.log('Skipping autoprune for ' + func.functionName + ' because it has no aliases defined.');
+          SCli.log('Skipping autoprune for ' + functionName + ' because it has no aliases defined.');
           return;
         }
         aliases.map(function (alias) {
@@ -156,14 +248,14 @@ module.exports = function(ServerlessPlugin, serverlessPath) { // Always pass in 
           if (version.Version.match(/^\d+$/)) {
             if (!aliasMap[version.Version]) {
               promise = promise.then(function () {
-                SCli.log('Autopruning ' + func.functionName + ' version ' + version.Version);
+                SCli.log('Autopruning ' + functionName + ' version ' + version.Version);
                 return lambda.deleteFunctionPromise({
-                  FunctionName: func.functionName,
+                  FunctionName: functionName,
                   Qualifier: version.Version
                 });
               });
             } else {
-              SCli.log('Keeping ' + func.functionName + ' version ' + version.Version + ' (has alias ' + aliasMap[version.Version] + ')');
+              SCli.log('Keeping ' + functionName + ' version ' + version.Version + ' (has alias ' + aliasMap[version.Version] + ')');
             }
           }
         });
